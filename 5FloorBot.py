f5225ce3ac748d189dc499c91e14f712908693cb
truth <truth@changingtec.com>
2017-06-20 13:49:31 +0800

    修正 ID判斷大小寫的錯誤

--- 5FloorBot.py
+++ 5FloorBot.py
@@ -31,92 +31,92 @@ Retry = True
 
 Startup = True
 
-while Retry:
-    PTTCrawler = PTT.Crawler(ID, Password, False)
-    if not PTTCrawler.isLoginSuccess():
-        PTTCrawler.Log('Login fail')
-    else:
-        #PTTCrawler.setLogLevel(PTTCrawler.LogLevel_DEBUG)
-        LastIndex = 0
-        LastIndexList = [0]
-        
-        NoFastPushWait = False
+PTTCrawler = PTT.Crawler(ID, Password, False)
+if not PTTCrawler.isLoginSuccess():
+    PTTCrawler.Log('Login fail')
+else:
+    #PTTCrawler.setLogLevel(PTTCrawler.LogLevel_DEBUG)
+    LastIndex = 0
+    LastIndexList = [0]
+    
+    NoFastPushWait = False
 
-        PTTCrawler.Log('Start detect new post in ' + Board)
-        while Retry:
-            try:
-                    
-                if not len(LastIndexList) == 0:
-                    LastIndex = LastIndexList.pop()
-                ErrorCode, LastIndexList = PTTCrawler.getNewPostIndexList(Board, LastIndex)
-                if ErrorCode != PTTCrawler.Success:
-                    PTTCrawler.Log('Get newest list error: ' + str(ErrorCode))
-                    time.sleep(1)
+    PTTCrawler.Log('Start detect new post in ' + Board)
+    while Retry:
+        try:
+                
+            if not len(LastIndexList) == 0:
+                LastIndex = LastIndexList.pop()
+            ErrorCode, LastIndexList = PTTCrawler.getNewPostIndexList(Board, LastIndex)
+            if ErrorCode != PTTCrawler.Success:
+                PTTCrawler.Log('Get newest list error: ' + str(ErrorCode))
+                time.sleep(1)
+                continue
+            
+            if not len(LastIndexList) == 0:
+                PTTCrawler.Log('Detected ' + str(len(LastIndexList)) + ' new post')
+                
+                if Startup:
+                    PTTCrawler.Log('Pass the post alread exist')
+                    Startup = False
                     continue
-                
-                if not len(LastIndexList) == 0:
-                    PTTCrawler.Log('Detected ' + str(len(LastIndexList)) + ' new post')
+                for NewPostIndex in LastIndexList:
+            
+                    PTTCrawler.Log('Detected ' + str(NewPostIndex))
                     
-                    if Startup:
-                        PTTCrawler.Log('Pass the post alread exist')
-                        Startup = False
+                    ErrorCode, Post = PTTCrawler.getPostInfoByIndex(Board, NewPostIndex)
+                    if ErrorCode == PTTCrawler.PostDeleted:
+                        PTTCrawler.Log('Post has been deleted')
                         continue
-                    for NewPostIndex in LastIndexList:
-                
-                        PTTCrawler.Log('Detected ' + str(NewPostIndex))
+                    if ErrorCode == PTTCrawler.WebFormatError:
+                        PTTCrawler.Log('Web structure error')
+                        continue
+                    if ErrorCode != PTTCrawler.Success:
+                        PTTCrawler.Log('Get post by index fail')
+                        continue
+                    if Post == None:
+                        PTTCrawler.Log('Post is empty')
+                        continue
+                    #PTTCrawler.Log(Post.getPostContent())
+                    if ID.lower() in Post.getPostContent().lower() or ID.lower() in Post.getTitle().lower():
+                        PTTCrawler.Log('User is not allow push')
+                        continue
+                    
+                    if '五樓' in Post.getOriginalData() or '5樓' in Post.getOriginalData() or '5 樓' in Post.getOriginalData():
+                        PTTCrawler.Log('Detect 5 floor trap')
+                        continue
                         
-                        ErrorCode, Post = PTTCrawler.getPostInfoByIndex(Board, NewPostIndex)
-                        if ErrorCode == PTTCrawler.PostDeleted:
-                            PTTCrawler.Log('Post has been deleted')
-                            continue
-                        if ErrorCode == PTTCrawler.WebFormatError:
-                            PTTCrawler.Log('Web structure error')
-                            continue
-                        if ErrorCode != PTTCrawler.Success:
-                            PTTCrawler.Log('Get post by index fail')
-                            continue
-                        if Post == None:
-                            PTTCrawler.Log('Post is empty')
-                            continue
-                        #PTTCrawler.Log(Post.getPostContent())
-                        if ID in Post.getPostContent():
-                            PTTCrawler.Log('User is not allow push')
-                            continue
+                    for i in range(len(Post.getPushList()), 5):
+                        PushContent = PushList[str(i + 1)]
+                        #PTTCrawler.Log('Push: ' + PushContent)
+                        ErrorCode = PTTCrawler.pushByIndex(Board, PTTCrawler.PushType_Push, PushContent, NewPostIndex)
                         
-                        if '五樓' in Post.getOriginalData() or '5樓' in Post.getOriginalData() or '5 樓' in Post.getOriginalData():
-                            PTTCrawler.Log('Detect 5 floor trap')
-                            continue
-                            
-                        for i in range(len(Post.getPushList()), 5):
-                            PushContent = PushList[str(i + 1)]
-                            #PTTCrawler.Log('Push: ' + PushContent)
-                            ErrorCode = PTTCrawler.pushByIndex(Board, PTTCrawler.PushType_Push, PushContent, NewPostIndex)
-                            
-                            if ErrorCode == PTTCrawler.Success:
-                                #PTTCrawler.Log('Push success')
-                                pass
-                            else:
-                                PTTCrawler.Log('Push fail')
-                                break
-                        PTTCrawler.Log('Index ' + str(NewPostIndex) + ' complete')
-            except KeyboardInterrupt:
-                '''
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                '''
-                PTTCrawler.Log('Interrupted by user')
-                PTTCrawler.logout()
-                sys.exit()
-            except EOFError:
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                Retry = True
-                break
-            except ConnectionAbortedError:
-                Retry = True
-                break
-            except Exception:
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                Retry = True
-                break
+                        if ErrorCode == PTTCrawler.Success:
+                            #PTTCrawler.Log('Push success')
+                            pass
+                        else:
+                            PTTCrawler.Log('Push fail')
+                            break
+                    PTTCrawler.Log('Index ' + str(NewPostIndex) + ' complete')
+        except KeyboardInterrupt:
+            '''
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            '''
+            PTTCrawler.Log('Interrupted by user')
+            PTTCrawler.logout()
+            sys.exit()
+        except EOFError:
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            Retry = True
+            break
+        except ConnectionAbortedError:
+            Retry = True
+            break
+        except Exception:
+            exc_info = sys.exc_info()
+            traceback.print_exception(*exc_info)
+            Retry = True
+            break
+    
